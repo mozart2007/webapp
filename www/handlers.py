e8b4c8191b20c1108e2bd63dbef838d2762d990c
mozart2007 <mozart2007@126.com>
2017-10-13 12:48:23 +0800

    debug01

--- www/handlers.py
+++ www/handlers.py
@@ -128,7 +128,7 @@ def api_register_user(*, email, name, passwd):
         raise APIValueError('passwd')
     users = yield from User.findAll('email=?', [email])
     if len(users) > 0:
-        raise APIError('register:failed', 'email', 'Email is already in use.')
+        raise APIValueError('email', 'Email已被占用！')
     uid = next_id()
     sha1_passwd = '%s:%s' % (uid, passwd)
     user = User(id=uid, name=name.strip(), email=email, passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(), image='http://www.gravatar.com/avatar/%s?d=mm&s=120' % hashlib.md5(email.encode('utf-8')).hexdigest())
